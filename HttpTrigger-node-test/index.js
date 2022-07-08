e2c5a8651d8f1d1733defb6bcfa62fc90cc3cb3e
vidigummy <vigigummy@gmail.com>
2022-07-08 19:38:29 +0900

    event sample directory

--- HttpTrigger-node-test/index.js
+++ HttpTrigger-node-test/index.js
@@ -1,6 +1,7 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    context.log("---------------------------------\n"+JSON.stringify(req.body)+"\n---------------------------------\n");
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully. 날 봐 날 봐 귀순"
@@ -11,5 +12,5 @@ module.exports = async function (context, req) {
         body: responseMessage
     };
 
-    
+
 }
