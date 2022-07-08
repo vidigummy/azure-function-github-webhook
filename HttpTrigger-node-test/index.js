8fe0677f5e13049444d787c169ff8b33a7ea24d5
vidigummy <vigigummy@gmail.com>
2022-07-08 17:31:07 +0900

    test 완료. 개발 시작

--- HttpTrigger-node-test/index.js
+++ HttpTrigger-node-test/index.js
@@ -3,11 +3,13 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully. 날 봐 날 봐 귀순"
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. 날 봐 날 봐 귀순";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+
+    
 }
